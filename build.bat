0b7b20780f627bbe650993671d9601ecfb39e8f1
melgrin <flourhorn@gmail.com>
2024-03-03 00:39:33 -0500

    Preliminary search boxes.
    They don't actually do what the label says they do, yet.

--- build.bat
+++ build.bat
@@ -4,7 +4,8 @@ if not exist bin mkdir bin
 pushd bin
 
 @echo on
-cl -nologo -MT -Gm- -GR- -EHa- -Od -Oi -W4 -WX -wd4201 -wd4189 -wd4100 -Z7 %* -Fe:input_duplicator ..\src\main.c /link -opt:ref user32.lib gdi32.lib winmm.lib
+:: cl -nologo -MT -Gm- -GR- -EHa- -Od -Oi -W4 -WX -wd4201 -wd4189 -wd4100 -Z7 %* -Fe:input_duplicator ..\src\main.c /link /manifest:embed -opt:ref user32.lib gdi32.lib winmm.lib
+cl -nologo -MT -Gm- -GR- -EHa- -Od -Oi -W4 -WX -wd4201 -wd4189 -wd4100 -Z7 %* -Fe:input_duplicator ..\src\main.c /link /manifest:embed
 @echo off
 
 popd
--- src/main.c
+++ src/main.c
@@ -4,9 +4,24 @@
 #include <windowsx.h> // HANDLE_MSG
 #include <stdio.h>
 #include <stdlib.h> // realloc
+#include <commctrl.h> // WC_EDIT
+
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib") // GetStockObject
+//#pragma comment(lib, "comctl32.lib")
+
+// This loads Common Controls Version 6.0, which has more modern visual styles.
+// Use linker option /manifest:embed to embed this into the exe, otherwise look for *.exe.manifest.
+// The publicKeyToken is specifically for Common Controls as dictated by Microsoft, and has nothing to do with this application.
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 HINSTANCE g_hinst;
 //HWND g_hwndChild;
+HWND g_hwndIncludeLabel;
+HWND g_hwndIncludeEdit;
+HWND g_hwndExcludeLabel;
+HWND g_hwndExcludeEdit;
+HWND g_hwndMatchCount;
 
 typedef struct {
     size_t count;
@@ -27,14 +42,111 @@ static HWNDs g_hwnd_targets;
     if (ok) (a).data[(a).count++] = it; \
 } while (0);
 
-void OnSize(HWND hwnd, UINT state, int cx, int cy) {
-    //if (g_hwndChild) {
-    //    MoveWindow(g_hwndChild, 0, 0, cx, cy, TRUE);
-    //}
+BOOL CALLBACK EnumWindows_MatchWindowClass(HWND hwnd, LPARAM lParam) {
+    static char class_name[512];
+    int numChars = GetClassName(hwnd, class_name, (int) sizeof(class_name));
+    if (numChars > 0) {
+        char* desired_class_name = (char*) lParam;
+        if (0 == strcmp(desired_class_name, class_name)) {
+            da_append(g_hwnd_targets, hwnd);
+        }
+    //} else {
+    //    DWORD err = GetLastError();
+    }
+    return TRUE; // keep enumerating
+}
+
+static void set_match_count(size_t n) {
+    static char buf[8];
+    snprintf(buf, sizeof(buf), "%zu", n);
+    SetWindowText(g_hwndMatchCount, buf);
 }
 
 BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpcs) {
+
+    g_hwndIncludeEdit = CreateWindow/*Ex*/(
+        //WS_EX_CLIENTEDGE,
+        WC_EDIT,
+        TEXT(""),
+        WS_CHILD | WS_VISIBLE | WS_BORDER,
+        0,0,0,0, // see WM_SIZE
+        hwnd, // parent
+        NULL,
+        g_hinst,
+        NULL);
+    if (!g_hwndIncludeEdit) return FALSE;
+
+    g_hwndIncludeLabel = CreateWindow(
+        WC_STATIC,
+        TEXT("Some Label"),
+        WS_CHILD | WS_VISIBLE | SS_CENTER /*| SS_SIMPLE*/,
+        0,0,0,0, // see WM_SIZE
+        hwnd, // parent
+        NULL,
+        g_hinst,
+        NULL);
+    if (!g_hwndIncludeLabel) return FALSE;
+
+    g_hwndExcludeEdit = CreateWindow/*Ex*/(
+        //WS_EX_CLIENTEDGE,
+        WC_EDIT,
+        TEXT(""),
+        WS_CHILD | WS_VISIBLE | WS_BORDER,
+        0,0,0,0, // see WM_SIZE
+        hwnd, // parent
+        NULL,
+        g_hinst,
+        NULL);
+    if (!g_hwndExcludeEdit) return FALSE;
+
+    g_hwndExcludeLabel = CreateWindow(
+        WC_STATIC,
+        TEXT("Some Label"),
+        WS_CHILD | WS_VISIBLE /*| SS_SIMPLE*/,
+        0,0,0,0, // see WM_SIZE
+        hwnd, // parent
+        NULL,
+        g_hinst,
+        NULL);
+    if (!g_hwndExcludeLabel) return FALSE;
+
+    g_hwndMatchCount = CreateWindow( // this is probably temporary (TODO)
+        WC_STATIC,
+        TEXT("0"),
+        WS_CHILD | WS_VISIBLE,
+        0,0,0,0, // see WM_SIZE
+        hwnd, // parent
+        NULL,
+        g_hinst,
+        NULL);
+    if (!g_hwndMatchCount) return FALSE;
+
+    // Somehow, the DEFAULT_GUI_FONT is not the default font, so tell the child windows to use it.
+    HGDIOBJ font = GetStockObject(DEFAULT_GUI_FONT);
+    SendMessage(g_hwndIncludeLabel, WM_SETFONT, (LPARAM) font, TRUE);
+    SendMessage(g_hwndIncludeEdit,  WM_SETFONT, (LPARAM) font, TRUE);
+    SendMessage(g_hwndExcludeLabel, WM_SETFONT, (LPARAM) font, TRUE);
+    SendMessage(g_hwndExcludeEdit,  WM_SETFONT, (LPARAM) font, TRUE);
+
     return TRUE;
+}
+
+void OnSize(HWND hwnd, UINT state, int cx, int cy) {
+    int input_region_x = cx / 3;
+    int input_region_y = cy / 3;
+    /// MoveWindow(hwnd, x, y, w, h, repaint)
+    const int h = 20;
+    const int pad = 10;
+    const int labelw = 60;
+    const int editw = 100;
+    MoveWindow(g_hwndIncludeLabel, pad,          pad, labelw, h, TRUE);
+    MoveWindow(g_hwndIncludeEdit,  pad + labelw, pad, editw,  h, TRUE);
+
+    MoveWindow(g_hwndExcludeLabel, pad,          pad + h, labelw, h, TRUE);
+    MoveWindow(g_hwndExcludeEdit,  pad + labelw, pad + h, editw,  h, TRUE);
+
+    MoveWindow(g_hwndMatchCount, pad, pad + (h * 2), labelw, h, TRUE);
+
 }
 
 void OnDestroy(HWND hwnd) {
@@ -92,7 +204,31 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uiMsg, WPARAM wParam, LPARAM lParam) {
                 }
             }
         } return 0;
+
+        case WM_COMMAND: {
+            if (lParam) {
+                HWND edit = (HWND) lParam;
+                if (edit == g_hwndIncludeEdit || edit == g_hwndExcludeEdit) {
+                    WORD edit_control_notification_code = HIWORD(wParam);
+                    if (edit_control_notification_code == EN_CHANGE) {
+                        static char buf[512];
+                        int n = GetWindowText(edit, buf, sizeof(buf));
+                        if (n > 0 && (size_t) n < sizeof(buf)) {
+                            //FIXME appends forever (just delete the last char of the match and retype it to see what I mean)
+                            //FIXME probably inefficient to enumerate all windows every time, but what's a girl to do??
+                            EnumWindows(EnumWindows_MatchWindowClass, (LPARAM) buf);
+                            set_match_count(g_hwnd_targets.count);
+                            //TODO actually display the matches
+                        }
+                        return 0;
+                    }
+                }
+
+            }
+            int _bp = 0;
+        } return 0;
     }
+
 
     return DefWindowProc(hwnd, uiMsg, wParam, lParam);
 }
@@ -115,23 +251,11 @@ BOOL InitApp() {
     return TRUE;
 }
 
-BOOL CALLBACK EnumWindows_MatchWindowClass(HWND hwnd, LPARAM lParam) {
-    static char class_name[512];
-    int numChars = GetClassName(hwnd, class_name, (int) sizeof(class_name));
-    if (numChars > 0) {
-        char* desired_class_name = (char*) lParam;
-        if (0 == strcmp(desired_class_name, class_name)) {
-            da_append(g_hwnd_targets, hwnd);
-        }
-    //} else {
-    //    DWORD err = GetLastError();
-    }
-    return TRUE; // keep enumerating
-}
+int WINAPI WinMain(
+    HINSTANCE hinst,
+    HINSTANCE hinstPrev,
+    LPSTR lpCmdLine, int nShowCmd) {
 
-int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hinstPrev,
-    LPSTR lpCmdLine, int nShowCmd)
-{
     MSG msg;
     HWND hwnd;
     g_hinst = hinst;
@@ -141,7 +265,7 @@ int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hinstPrev,
         TEXT(NAME),                     // Title
         WS_OVERLAPPEDWINDOW,            // Style
         CW_USEDEFAULT, CW_USEDEFAULT,   // Position
-        200, 100,                       // Size // there's nothing in it right now, so it can be small
+        200, 200,                       // Size
         NULL,                           // Parent
         NULL,                           // No menu
         hinst,                          // Instance
@@ -149,6 +273,7 @@ int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hinstPrev,
     ShowWindow(hwnd, nShowCmd);
 
     EnumWindows(EnumWindows_MatchWindowClass, (LPARAM) "RenderTestWindowClass");
+    set_match_count(g_hwnd_targets.count);
 
     //TODO show this info in the application's window
     //char buf[128];
